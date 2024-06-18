e95585c89cbbcbb42f5cecc6ac9e7fe23fc90aa6
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-06-18 16:12:30 -0300

    primeiro commit

---
+++ src/main/java/com/jeffsenaa/apispring/ApiSpringNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.jeffsenaa.apispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffsenaa/apispring/ApiSpringNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffsenaa.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
