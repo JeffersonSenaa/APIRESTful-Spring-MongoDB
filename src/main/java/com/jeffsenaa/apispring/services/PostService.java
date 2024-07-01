a45c40a9bac9eb5b93a20161d39ded077af4c9ce
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-06-30 22:54:31 -0300

    Consulta com Query Methods

--- src/main/java/com/jeffsenaa/apispring/services/PostService.java
+++ src/main/java/com/jeffsenaa/apispring/services/PostService.java
@@ -25,4 +25,9 @@ public class PostService {
 		Optional<Post> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContaining(text);
+	}
 }
+ 
