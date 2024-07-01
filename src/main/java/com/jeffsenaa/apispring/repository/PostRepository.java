be9b3c055a533d643707ea24b87e6f4d8257c3d2
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-06-30 23:05:34 -0300

    Consulta com @Query e regex para configurar expressão regular

--- src/main/java/com/jeffsenaa/apispring/repository/PostRepository.java
+++ src/main/java/com/jeffsenaa/apispring/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.jeffsenaa.apispring.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jeffsenaa.apispring.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	//query methods
 	List<Post> findByTitleContainingIgnoreCase(String text);
