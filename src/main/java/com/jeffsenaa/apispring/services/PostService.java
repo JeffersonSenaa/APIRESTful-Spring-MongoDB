e2475f03db37d62ee1e3e5f44f15f3a0deb23293
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-06-30 21:59:00 -0300

    Implementando Camadas de Resource e Service para Post

---
+++ src/main/java/com/jeffsenaa/apispring/services/PostService.java
@@ -0,0 +1,28 @@
+package com.jeffsenaa.apispring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeffsenaa.apispring.domain.Post;
+import com.jeffsenaa.apispring.domain.User;
+import com.jeffsenaa.apispring.repository.PostRepository;
+import com.jeffsenaa.apispring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<User> findAll(){
+		return findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
